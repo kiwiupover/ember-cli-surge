89348e9274f181817f969c3f82062e9721d4f4f6
Dave Laird <kiwiupover@gmail.com>
2017-06-18 23:13:47 -0700

    Adding ember-cli-update Adding eslint Adding eslint npm script commands

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  plugins: [
+    'mocha'
+  ],
+  extends: 'sane',
+  env: {
+    es6: true,
+    node: true
+  },
+  rules: {
+    'mocha/no-exclusive-tests': 'error'
+  }
+}
